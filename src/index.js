28b11be0f6bbd85e4fe86e999f0f747a69ca31a7
Aayush Jain <75216351+Aayushjain-code@users.noreply.github.com>
2021-05-26 17:26:14 +0530

    key props

--- src/index.js
+++ src/index.js
@@ -9,12 +9,14 @@ import './index.css';
 //converted different objects into single one for "proper list"
 const books = [
   {
+    id: 1,
     author: "John Doe",
     title: "I love You to the moon and Backssss!",
     image: "https://images-na.ssl-images-amazon.com/images/I/91JhcC33dTL.__AC_SX300_SY300_QL70_FMwebp_.jpg",
 
   },
   {
+    id: 2,
     author: "Ammaya helli",
     title: "mobiles are awesome",
     image: "https://m.media-amazon.com/images/I/51aIDYrc84L._AC_SY200_.jpg",
@@ -27,11 +29,11 @@ function BookList() {
   return (
     <section className="bookList">
       {books.map((book) => {
-        {/* console.log(book);
-        const { image, author, title } = book; */}
-
         return (
-          <Book book={book}></Book>
+          <Book
+            key={book.id}
+            book={book}
+          ></Book>
         )
       })}
     </section>
@@ -45,7 +47,6 @@ const Book = (props) => {
       <img src={image} />
       <h1>{title}</h1>
       <h4 >{author}</h4>
-
     </article>
   )
 }
