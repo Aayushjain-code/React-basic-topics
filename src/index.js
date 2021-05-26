a132b914bb1ab2754c09bb8ed0fe3ed44d6baa90
Aayush Jain <75216351+Aayushjain-code@users.noreply.github.com>
2021-05-26 10:51:43 +0530

    JSX-JS

--- src/index.js
+++ src/index.js
@@ -17,26 +17,24 @@ function BookList() {
     </section>
   )
 }
-
+//const can be declared out
+const author = "John Doe";
 
 const Book = () => {
-  return <article className="book">
-    <Image />
-    <Title />
-    <Author />
-  </article>
+  //const can be declared in
+  const title = "I love You to the moon and Backssss!"
+
+  return (
+    <article className="book">
+      <img src="https://images-na.ssl-images-amazon.com/images/I/91JhcC33dTL.__AC_SX300_SY300_QL70_FMwebp_.jpg" />
+      <h1>{title}</h1>
+      <h4 >{author.toUpperCase()}</h4>
+      {/* jsx me object pass krna padta hai to return something */}
+      {/* <p>{let x = 6}</p> */}
+      <p>{6 + 6}</p>
+    </article>
+  )
 }
-
-
-const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/91JhcC33dTL.__AC_SX300_SY300_QL70_FMwebp_.jpg" />
-
-
-const Title = () => <h1>This is the Title</h1>;
-
-function Author() {
-  return <h4 style={{ color: "red", fontSize: "0.75rem", marginTop: "0.25rem" }}>John Doe</h4>
-}
-
 
 
 ReactDOM.render(
