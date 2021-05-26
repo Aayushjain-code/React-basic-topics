95f8f7a7d80be4f4359c4c0af407ee21263676d9
Aayush Jain <75216351+Aayushjain-code@users.noreply.github.com>
2021-05-26 19:08:18 +0530

    Import and exports

---
+++ src/Books.js
@@ -0,0 +1,16 @@
+export const data = [
+	{
+		id: 1,
+		author: "John Doe",
+		title: "I love You to the moon and Backssss!",
+		image: "https://images-na.ssl-images-amazon.com/images/I/91JhcC33dTL.__AC_SX300_SY300_QL70_FMwebp_.jpg",
+
+	},
+	{
+		id: 2,
+		author: "Ammaya helli",
+		title: "mobiles are awesome",
+		image: "https://m.media-amazon.com/images/I/51aIDYrc84L._AC_SY200_.jpg",
+
+	},
+];
--- src/index.js
+++ src/index.js
@@ -1,34 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
 //css
 import './index.css';
-//setup vars
 
-//converted different objects into single one for "proper list"
-const books = [
-  {
-    id: 1,
-    author: "John Doe",
-    title: "I love You to the moon and Backssss!",
-    image: "https://images-na.ssl-images-amazon.com/images/I/91JhcC33dTL.__AC_SX300_SY300_QL70_FMwebp_.jpg",
 
-  },
-  {
-    id: 2,
-    author: "Ammaya helli",
-    title: "mobiles are awesome",
-    image: "https://m.media-amazon.com/images/I/51aIDYrc84L._AC_SY200_.jpg",
-
-  },
-];
+//importing custom
+//not a default exported function therefore using { }
+import { data } from './Books';
+//default exported function
+import Book from "./Book"
 
 
 function BookList() {
   return (
     <section className="bookList">
-      {books.map((book) => {
+      {data.map((book) => {
         return (
           <Book
             key={book.id}
@@ -40,23 +27,7 @@ function BookList() {
   )
 }
 
-const Book = ({ image, title, author }) => {
 
-  //attribute,eventHandler
-  //onClick,onMouseHover
-
-
-  return (
-    <article className="book" onMouseOver={() => {
-      console.log(author);
-    }}>
-      <img src={image} alt="" />
-
-      <h1>{title}</h1>
-      <h4 >{author}</h4>
-    </article>
-  )
-}
 
 
 ReactDOM.render(
